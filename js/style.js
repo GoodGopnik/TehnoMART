7f78b52cad5417f3bb8656aec485015a26b8ca0c
GoodGopnik <https://github.com/GoodGopnik/work.git>
2020-08-12 01:07:06 +0300

    Сделал бургер и адаптацию

--- js/style.js
+++ js/style.js
@@ -2,9 +2,6 @@ $('.icon-search').click(function (e) {
     $('.input-serch, .icon-search').toggleClass('show-input');
 });
 
-$('mini-menu').click(function () {
-    $('.menu ul').slideToggle();
+$('.mini-menu').click(function () {
+    $('.menu ul').toggleClass('show');
 });
-// $('mini-menu').click(function () {
-//     $('.menu ul').slideToggle();
-// });
