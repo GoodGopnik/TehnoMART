b9ff8ab7a445f408a3369c717f8ee50d1b30bbf1
GoodGopnik <https://github.com/GoodGopnik/work.git>
2020-08-12 00:59:12 +0300

    Сделал меню и бургер но не рабочее

--- js/style.js
+++ js/style.js
@@ -1,3 +1,10 @@
 $('.icon-search').click(function (e) {
     $('.input-serch, .icon-search').toggleClass('show-input');
-})
+});
+
+$('mini-menu').click(function () {
+    $('.menu ul').slideToggle();
+});
+// $('mini-menu').click(function () {
+//     $('.menu ul').slideToggle();
+// });
